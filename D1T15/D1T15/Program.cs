7aea8b0934b5548b75bd8bc68941e1584ba4fd10
H8872 <H8872@student.jamk.fi>
2016-01-13 14:46:31 +0200

    Initial commit.

---
+++ D1T15/D1T15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1T15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna Luku > ");
+        }
+    }
+}

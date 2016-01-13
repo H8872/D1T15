cef7a1c6db7d3cc0ed26f32bd658ceacb975de7d
H8872 <H8872@student.jamk.fi>
2016-01-13 15:33:32 +0200

    More code, topside is upside down...

--- D1T15/D1T15/Program.cs
+++ D1T15/D1T15/Program.cs
@@ -10,7 +10,51 @@ namespace D1T15
     {
         static void Main(string[] args)
         {
+            int luku, valil=0;
             Console.Write("Anna Luku > ");
+            luku = int.Parse(Console.ReadLine());
+            valil = luku - 3;
+            for(int i=luku; i>=3; i--)
+            {
+                for(int j=i-3; j>0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = luku-3; k>0; k--)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine("*");
+
+                luku--;
+            }
+
+
+            if (luku == 2)
+            {
+                for(int i = valil; i > 0; i--)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("*");
+                luku--;
+            }
+            if(luku == 1)
+            {
+                for (int i = valil; i > 0; i--)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("*");
+            }
+            else
+            {
+                Console.WriteLine("-");
+            }
+
+            Console.ReadLine();
         }
     }
 }
